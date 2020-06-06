7e70ba1381649cc852775ea86ed7f2b660fb370b
workkk <gentlebob1998@icloud.com>
2020-06-06 14:28:51 +0800

    feat: 主页优化 & renderwatcher

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,14 +7,26 @@ module.exports = {
       { text: 'Github主页', link: 'https://github.com/workINgithub'}
     ],
     sidebar: [
-      '/',
+      {
+        title: '介绍',
+        path: '/guide/'
+      },
       {
         title: 'npm',   // 必要的
-        // path: '/npm/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        path: '/npm/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
         collapsable: false, // 可选的, 默认值是 true,
         sidebarDepth: 1,    // 可选的, 默认值是 1,
         children: [
           ['/npm/npm-scripts', 'npm-scripts']
+        ]
+      },
+      {
+        title: 'vue',   // 必要的
+        path: '/vue/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1,
+        children: [
+          ['/vue/renderwatcher', '渲染观察者']
         ]
       },
     ],
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,13 @@
-# 介绍
+---
+home: true
+heroImage: /hero.jpeg
+heroText: 小凡的博客
+tagline: null
+actionText: 开始 →
+actionLink: /guide/
+# features:
+# - title: Vue
+#   details: 解析Vue源码。
+# - title: npm
+#   details: 了解npm生态，进阶前端工程化。
+---
