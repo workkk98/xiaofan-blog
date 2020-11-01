fa2fa915d602eb6a7f66b2d1f2951927138ced59
workkk <gentlebob1998@icloud.com>
2020-11-01 18:37:40 +0800

    docs: 从VNode到DOM

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -26,7 +26,8 @@ module.exports = {
         collapsable: false, // 可选的, 默认值是 true,
         sidebarDepth: 1,    // 可选的, 默认值是 1,
         children: [
-          ['/vue/renderwatcher', '渲染观察者']
+          ['/vue/renderwatcher', '渲染观察者'],
+          ['/vue/VNode_DOM', '从VNode到DOM']
         ]
       },
     ],
